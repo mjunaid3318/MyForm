9a310b1906208df0d71a6bcb187cbd4af566e15d
Muhammad junaid <122457045+mjunaid3318@users.noreply.github.com>
2023-01-12 11:54:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var input1 = document.getElementById("inp1");
+var input2 = document.getElementById("inp2");
+var btn = document.getElementById("submit");
+btn.addEventListener("click");
+function getFormvalue(){
+    let 
+}
